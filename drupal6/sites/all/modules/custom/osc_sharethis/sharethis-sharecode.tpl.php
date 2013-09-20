77fa7f911258cb7d7c931fbba69e54b1bedbca46
bgonzal <jason@Stevens-Mac-mini.local>
2013-09-20 13:43:07 -0600

    Add Drupal6
    Iniciar el envio del modulo sharethis

---
+++ drupal6/sites/all/modules/custom/osc_sharethis/sharethis-sharecode.tpl.php
@@ -0,0 +1,9 @@
+<div id="sharethis-main-items">
+  
+  <span class='st_linkedin_custom' st_url="http://www.linkedin.com/" title="LinkedIn" st_title="Sharing Rocks!"></span>
+  <span class='st_twitter_custom'  st_url='https://twitter.com/' title="Twitter"></span>
+  <span class='st_facebook_custom' st_url='http://www.facebook.com/Telenoticias7' ></span>
+  <span class='st_email_custom' st_url='https://www.facebook.com/' title="Email"></span>
+  <span class='st_sharethis_custom'></span>
+   
+</div>
